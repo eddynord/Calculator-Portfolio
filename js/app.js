c286a60b4dfd153b7ff21fa5b574edb001d629c2
Edwin Nord <edwin.nord09@gmail.com>
2022-06-26 22:43:08 -0400

    Js Update

--- js/app.js
+++ js/app.js
@@ -1,47 +1,74 @@
-let keys = document.querySelector('.calculator');
-let display = document.querySelector('.form-control');
-let calculator = document.querySelector('.container-fluid')
+var input = document.querySelector('.form-control');
+numberKey = document.querySelector('.g-col-6');
+operators = document.getElementById('operator');
+equal = document.getElementById('equal');
+clearBtn = document.getElementById('clear');
+displayedResult = false;
 
-keys.addEventListener('click', event => {
-    if(!event.target.closest('button')) return
+for(var i = 0 ; i < numberKey.length; i++) {
+    numberKey[i].add.EventListener('click', function(e) {
+        var currentString = input.innerHTML;
+        var lastChar = currentString[currentString.length - 1];
 
-    const key = event.target
-    const keyValue = key.textContent
-    const displayValue = display.textContent
-    const { type } = key.dataset
-    const { previousKeyType } = calculator.dataset
-    // display.textContent = keyValue
-
-    if(type === 'number'){
-        if( displayValue === '0' || previousKeyType === 'operator') {
-            display.textContent = keyValue
+        if(displayedResult === false) {
+            input.innerHTML += e.target.innerHTML;
+        } else if (displayedResult === true && lastChar === "+" || lastChar === "-" || lastChar === "x" || lastChar === "÷") {
+            displayedResult = false;
+            input.innerHTML += e.target.innerHTML;
         } else {
-            display.textContent = displayValue = keyValue
+            displayedResult = false;
+            input.innerHTML = "";
+            input.innerHTML += e.target.innerHTML; 
         }
+    });
+}
+
+    equal.addEventListener("click" , function() {
+        var inputString = input.innerHTML;
+        var numbers = inputString.split(/\+|\-|\×|\÷/g);
+        var operator = inputString.replace(/[0-9]|\./g, "").split("");
+
+        console.log(inputString);
+        console.log(operator);
+        console.log(numbers);
+        console.log("----------------------------");
+
+        var divide = operators.indexOf("÷");
+            while (divide != -1) {
+                numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+                operator.splice(divide, 1);
+                divide = operators.indexOf("÷");
     }
 
-    if (type === 'equal') {
-        // Perform a calculation
-        const firstNumber = calculator.dataset.firstNumber
-        const operator = calculator.dataset.operator
-        const secondNumber = displayValue
-        display.textContent = calculate(firstNumber, operator, secondNumber)
-      }
-    
-      calculator.dataset.previousKeyType = type
-    })
-    
-    function calculate (firstNumber, operator, secondNumber) {
-      firstNumber = parseInt(firstNumber)
-      secondNumber = parseInt(secondNumber)
-    
-      if (operator === 'plus') return firstNumber + secondNumber
-      if (operator === 'minus') return firstNumber - secondNumber
-      if (operator === 'times') return firstNumber * secondNumber
-      if (operator === 'divide') return firstNumber / secondNumber
+        var multiply = operators.indexOf("×");
+            while (multiply != -1) {
+                numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+                operator.splice(multiply, 1);
+                multiply = operators.indexOf("×");
     }
-    
-    
-    
+
+    var subtract = operators.indexOf("-");
+            while (subtract != -1) {
+                numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+                operator.splice(subtract, 1);
+                subtract = operators.indexOf("-");
+    }
+
+    var add = operators.indexOf("+");
+    while (add != -1) {
+        // using parseFloat is necessary, otherwise it will result in string concatenation :)
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operator.splice(add, 1);
+        add = operators.indexOf("+");
+    }
+
+    input.innerHTML = numbers[0]; // displaying the output
+
+    displayedResult = true; // turning flag if result is displayed
+    });
+
+    // clearing the input on press of clear
+    clear.addEventListener("click", function() {
+    input.innerHTML = "";
+
 })
-
